0028adc04c5f87e65c4b8d9294595e3c05d2e3b5
snozaki <snozaki@netprotections.co.jp>
2021-09-19 10:19:04 +0900

    serverless-offlineの導入

--- serverless.ts
+++ serverless.ts
@@ -9,7 +9,10 @@ const serverlessConfiguration: AWS = {
       includeModules: true,
     },
   },
-  plugins: ['serverless-webpack'],
+  plugins: [
+    'serverless-webpack',
+    'serverless-offline'
+  ],
   provider: {
     name: 'aws',
     runtime: 'nodejs14.x',
